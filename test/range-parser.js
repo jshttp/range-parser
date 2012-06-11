3db745dbcc62b82922f3d8bcafabe28fdd4f3b3c
TJ Holowaychuk <tj@vision-media.ca>
2012-06-11 09:06:02 -0700

    Initial commit

---
+++ test/range-parser.js
@@ -0,0 +1,15 @@
+
+var parse = require('..');
+
+describe('parseRange(len, str)', function(){
+  it('should parse range strings', function(){
+    parse(1000, 'bytes=0-499').should.eql([{ start: 0, end: 499 }]);
+    parse(1000, 'bytes=40-80').should.eql([{ start: 40, end: 80 }]);
+    parse(1000, 'bytes=-500').should.eql([{ start: 500, end: 999 }]);
+    parse(1000, 'bytes=-400').should.eql([{ start: 600, end: 999 }]);
+    parse(1000, 'bytes=500-').should.eql([{ start: 500, end: 999 }]);
+    parse(1000, 'bytes=400-').should.eql([{ start: 400, end: 999 }]);
+    parse(1000, 'bytes=0-0').should.eql([{ start: 0, end: 0 }]);
+    parse(1000, 'bytes=-1').should.eql([{ start: 999, end: 999 }]);
+  })
+})
