12c1461e7627c30a0bca5a114a2bf9447e77c7fa
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-10-26 21:03:33 -0400

    tests: use assert

--- test/range-parser.js
+++ test/range-parser.js
@@ -1,6 +1,6 @@
 
+var assert = require('assert')
 var parse = require('..')
-  , assert = require('assert');
 
 function arr(type, arr) {
   arr.type = type;
@@ -8,19 +8,75 @@ function arr(type, arr) {
 }
 
 describe('parseRange(len, str)', function(){
-  it('should parse range strings', function(){
-    assert(-1 == parse(200, 'bytes=500-20'));
-    assert(-2 == parse(200, 'malformed'));
-    parse(200, 'bytes=0-499').should.eql(arr('bytes', [{ start: 0, end: 199 }]));
-    parse(1000, 'bytes=0-499').should.eql(arr('bytes', [{ start: 0, end: 499 }]));
-    parse(1000, 'bytes=40-80').should.eql(arr('bytes', [{ start: 40, end: 80 }]));
-    parse(1000, 'bytes=-500').should.eql(arr('bytes', [{ start: 500, end: 999 }]));
-    parse(1000, 'bytes=-400').should.eql(arr('bytes', [{ start: 600, end: 999 }]));
-    parse(1000, 'bytes=500-').should.eql(arr('bytes', [{ start: 500, end: 999 }]));
-    parse(1000, 'bytes=400-').should.eql(arr('bytes', [{ start: 400, end: 999 }]));
-    parse(1000, 'bytes=0-0').should.eql(arr('bytes', [{ start: 0, end: 0 }]));
-    parse(1000, 'bytes=-1').should.eql(arr('bytes', [{ start: 999, end: 999 }]));
-    parse(1000, 'items=0-5').should.eql(arr('items', [{ start: 0, end: 5 }]));
-    parse(1000, 'bytes=40-80,-1').should.eql(arr('bytes', [{ start: 40, end: 80 }, { start: 999, end: 999 }]));
+  it('should return -2 for invalid str', function(){
+    assert.strictEqual(parse(200, 'malformed'), -2)
+  })
+
+  it('should return -1 for invalid range', function(){
+    assert.strictEqual(parse(200, 'bytes=500-20'), -1)
+  })
+
+  it('should parse str', function(){
+    var range = parse(1000, 'bytes=0-499')
+    assert.strictEqual(range.type, 'bytes')
+    assert.strictEqual(range.length, 1)
+    assert.deepEqual(range[0], { start: 0, end: 499 })
+  })
+
+  it('should cap end at size', function(){
+    var range = parse(200, 'bytes=0-499')
+    assert.strictEqual(range.type, 'bytes')
+    assert.strictEqual(range.length, 1)
+    assert.deepEqual(range[0], { start: 0, end: 199 })
+  })
+
+  it('should parse str', function(){
+    var range = parse(1000, 'bytes=40-80')
+    assert.strictEqual(range.type, 'bytes')
+    assert.strictEqual(range.length, 1)
+    assert.deepEqual(range[0], { start: 40, end: 80 })
+  })
+
+  it('should parse str asking for last n bytes', function(){
+    var range = parse(1000, 'bytes=-400')
+    assert.strictEqual(range.type, 'bytes')
+    assert.strictEqual(range.length, 1)
+    assert.deepEqual(range[0], { start: 600, end: 999 })
+  })
+
+  it('should parse str with only start', function(){
+    var range = parse(1000, 'bytes=400-')
+    assert.strictEqual(range.type, 'bytes')
+    assert.strictEqual(range.length, 1)
+    assert.deepEqual(range[0], { start: 400, end: 999 })
+  })
+
+  it('should parse str with no bytes', function(){
+    var range = parse(1000, 'bytes=0-0')
+    assert.strictEqual(range.type, 'bytes')
+    assert.strictEqual(range.length, 1)
+    assert.deepEqual(range[0], { start: 0, end: 0 })
+  })
+
+  it('should parse str asking for last byte', function(){
+    var range = parse(1000, 'bytes=-1')
+    assert.strictEqual(range.type, 'bytes')
+    assert.strictEqual(range.length, 1)
+    assert.deepEqual(range[0], { start: 999, end: 999 })
+  })
+
+  it('should parse str with multiple ranges', function(){
+    var range = parse(1000, 'bytes=40-80,-1')
+    assert.strictEqual(range.type, 'bytes')
+    assert.strictEqual(range.length, 2)
+    assert.deepEqual(range[0], { start: 40, end: 80 })
+    assert.deepEqual(range[1], { start: 999, end: 999 })
+  })
+
+  it('should parse non-byte range', function(){
+    var range = parse(1000, 'items=0-5')
+    assert.strictEqual(range.type, 'items')
+    assert.strictEqual(range.length, 1)
+    assert.deepEqual(range[0], { start: 0, end: 5 })
   })
 })
