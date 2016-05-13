b0542ea05474d334e73c0ff2bc5cb58e8ce261b8
Douglas Christopher Wilson <doug@somethingdoug.com>
2016-05-13 14:45:20 -0400

    lint: use standard style

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
  * MIT Licensed
  */
 
-'use strict';
+'use strict'
 
 /**
  * Module exports.
  * @public
  */
 
-module.exports = rangeParser;
+module.exports = rangeParser
 
 /**
  * Parse "Range" header `str` relative to the given file `size`.
@@ -22,43 +22,46 @@ module.exports = rangeParser;
  * @public
  */
 
-function rangeParser(size, str) {
-  var i = str.indexOf('=');
+function rangeParser (size, str) {
+  var i = str.indexOf('=')
 
-  if (-1 == i) return -2;
+  if (i === -1) {
+    return -2
+  }
 
-  var arr = str.slice(i + 1).split(',').reduce(function(memo, range){
-    var range = range.split('-')
-      , start = parseInt(range[0], 10)
-      , end = parseInt(range[1], 10);
+  var arr = str.slice(i + 1).split(',').reduce(function eachRange (memo, range) {
+    var parts = range.split('-')
+    var start = parseInt(parts[0], 10)
+    var end = parseInt(parts[1], 10)
 
     // -nnn
     if (isNaN(start)) {
-      start = size - end;
-      end = size - 1;
+      start = size - end
+      end = size - 1
     // nnn-
     } else if (isNaN(end)) {
-      end = size - 1;
+      end = size - 1
     }
 
     // limit last-byte-pos to current length
-    if (end > size - 1) end = size - 1;
+    if (end > size - 1) {
+      end = size - 1
+    }
 
     // invalid
-    if (isNaN(start)
-      || isNaN(end)
-      || start > end
-      || start < 0) return memo;
+    if (isNaN(start) || isNaN(end) || start > end || start < 0) {
+      return memo
+    }
 
     memo.push({
       start: start,
       end: end
-    });
+    })
 
-    return memo;
-  }, []);
+    return memo
+  }, [])
 
-  arr.type = str.slice(0, i);
+  arr.type = str.slice(0, i)
 
-  return arr.length ? arr : -1;
+  return arr.length ? arr : -1
 }
--- test/range-parser.js
+++ test/range-parser.js
@@ -2,74 +2,74 @@
 var assert = require('assert')
 var parse = require('..')
 
-describe('parseRange(len, str)', function(){
-  it('should return -2 for invalid str', function(){
+describe('parseRange(len, str)', function () {
+  it('should return -2 for invalid str', function () {
     assert.strictEqual(parse(200, 'malformed'), -2)
   })
 
-  it('should return -1 if all specified ranges are invalid', function(){
+  it('should return -1 if all specified ranges are invalid', function () {
     assert.strictEqual(parse(200, 'bytes=500-20'), -1)
     assert.strictEqual(parse(200, 'bytes=500-999'), -1)
     assert.strictEqual(parse(200, 'bytes=500-999,1000-1499'), -1)
   })
 
-  it('should parse str', function(){
+  it('should parse str', function () {
     var range = parse(1000, 'bytes=0-499')
     assert.strictEqual(range.type, 'bytes')
     assert.strictEqual(range.length, 1)
     assert.deepEqual(range[0], { start: 0, end: 499 })
   })
 
-  it('should cap end at size', function(){
+  it('should cap end at size', function () {
     var range = parse(200, 'bytes=0-499')
     assert.strictEqual(range.type, 'bytes')
     assert.strictEqual(range.length, 1)
     assert.deepEqual(range[0], { start: 0, end: 199 })
   })
 
-  it('should parse str', function(){
+  it('should parse str', function () {
     var range = parse(1000, 'bytes=40-80')
     assert.strictEqual(range.type, 'bytes')
     assert.strictEqual(range.length, 1)
     assert.deepEqual(range[0], { start: 40, end: 80 })
   })
 
-  it('should parse str asking for last n bytes', function(){
+  it('should parse str asking for last n bytes', function () {
     var range = parse(1000, 'bytes=-400')
     assert.strictEqual(range.type, 'bytes')
     assert.strictEqual(range.length, 1)
     assert.deepEqual(range[0], { start: 600, end: 999 })
   })
 
-  it('should parse str with only start', function(){
+  it('should parse str with only start', function () {
     var range = parse(1000, 'bytes=400-')
     assert.strictEqual(range.type, 'bytes')
     assert.strictEqual(range.length, 1)
     assert.deepEqual(range[0], { start: 400, end: 999 })
   })
 
-  it('should parse "bytes=0-"', function(){
+  it('should parse "bytes=0-"', function () {
     var range = parse(1000, 'bytes=0-')
     assert.strictEqual(range.type, 'bytes')
     assert.strictEqual(range.length, 1)
     assert.deepEqual(range[0], { start: 0, end: 999 })
   })
 
-  it('should parse str with no bytes', function(){
+  it('should parse str with no bytes', function () {
     var range = parse(1000, 'bytes=0-0')
     assert.strictEqual(range.type, 'bytes')
     assert.strictEqual(range.length, 1)
     assert.deepEqual(range[0], { start: 0, end: 0 })
   })
 
-  it('should parse str asking for last byte', function(){
+  it('should parse str asking for last byte', function () {
     var range = parse(1000, 'bytes=-1')
     assert.strictEqual(range.type, 'bytes')
     assert.strictEqual(range.length, 1)
     assert.deepEqual(range[0], { start: 999, end: 999 })
   })
 
-  it('should parse str with multiple ranges', function(){
+  it('should parse str with multiple ranges', function () {
     var range = parse(1000, 'bytes=40-80,-1')
     assert.strictEqual(range.type, 'bytes')
     assert.strictEqual(range.length, 2)
@@ -77,14 +77,14 @@ describe('parseRange(len, str)', function(){
     assert.deepEqual(range[1], { start: 999, end: 999 })
   })
 
-  it('should parse str with some invalid ranges', function(){
+  it('should parse str with some invalid ranges', function () {
     var range = parse(200, 'bytes=0-499,1000-,500-999')
     assert.strictEqual(range.type, 'bytes')
     assert.strictEqual(range.length, 1)
     assert.deepEqual(range[0], { start: 0, end: 199 })
   })
 
-  it('should parse non-byte range', function(){
+  it('should parse non-byte range', function () {
     var range = parse(1000, 'items=0-5')
     assert.strictEqual(range.type, 'items')
     assert.strictEqual(range.length, 1)
