da93ab26d640b2d50e8c1dbe1cf950437403211e
Blake Embrey <hello@blakeembrey.com>
2026-06-25 13:46:05 -0700

    Add -0 to test cases

--- test/range-parser.js
+++ test/range-parser.js
@@ -109,6 +109,11 @@ describe('parseRange(len, str)', function () {
     deepEqual(range[0], { start: 600, end: 999 })
   })
 
+  it('should be unsatisfiable asking for -0', function () {
+    var range = parse(1000, 'bytes=-0')
+    assert.strictEqual(range, -1)
+  })
+
   it('should use the whole representation when the suffix-length is larger', function () {
     var range = parse(100, 'bytes=-101')
     assert.strictEqual(range.type, 'bytes')
